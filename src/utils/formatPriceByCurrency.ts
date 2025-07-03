b1c077a7f4c8239aac4ddf5f2a1601c1aecc2177
Dmitry Arkhipov <arhidiman@mail.ru>
2025-07-03 18:43:49 +0700

    build

--- src/utils/formatPriceByCurrency.ts
+++
@@ -1,7 +0,0 @@
-export const formatPriceByCurrency = (price: number, currency: 'RUB' | 'USD' | 'EUR') => {
-    return new Intl.NumberFormat('ru-RU', {
-            style: 'currency',
-            currency,
-            minimumFractionDigits: 2
-    }).format(price / 100);
-}
